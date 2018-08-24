84e8d23fb3627261a9fe6d45a268feb48d6b825b
Daniela Giraldo <dannisgp2015@gmail.com>
2018-08-23 22:51:46 -0500

    login scriptweb

---
+++ loginscript/views/front/pages/login.php
@@ -0,0 +1,30 @@
+<?php 
+include ('../templates/head.php')
+?>
+
+<h1 class="titulo1"> Formulario Login</h1>
+
+<form action="http://localhost/loginscript/views/front/validarCode.php" class="formularios" method="post">
+
+<h2 class="titulo2"> Ingresar</h2>
+<div class="contenedor-inputs">
+  
+  <input type="email" name="correo_user" placeholder="Correo electronico" class="input-100 input-for" required>
+  
+  <input type="password" name="pass_user" placeholder="Contraseña" class="input-100 input-for" required>
+<p class="p_recordar">
+				<input type="checkbox" name="recordar" class="recordar">Recordar contraseña</input>
+			</p>
+
+  <input type="submit" value="Ingresar" class="btn-login">
+
+</div>
+
+</form>
+
+
+<?php 
+include ('../templates/foot.php')
+?>
+
+
